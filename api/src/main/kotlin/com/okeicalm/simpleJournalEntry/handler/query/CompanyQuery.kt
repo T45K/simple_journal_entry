31e5cc6fb1f42f1b4ce687f11f2a43d9aaf4103b
T45K <tasktas9@gmail.com>
2023-07-11 10:08:50 +0900

    Impled Read function

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/query/CompanyQuery.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/query/CompanyQuery.kt
@@ -3,10 +3,15 @@ package com.okeicalm.simpleJournalEntry.handler.query
 import com.expediagroup.graphql.generator.scalars.ID
 import com.expediagroup.graphql.server.operations.Query
 import com.okeicalm.simpleJournalEntry.handler.type.CompanyType
+import com.okeicalm.simpleJournalEntry.repository.CompanyRepository
+import com.okeicalm.simpleJournalEntry.util.toLong
 import org.springframework.stereotype.Component
 
 @Component
-class CompanyQuery : Query {
-    fun allCompanies(): List<CompanyType> = TODO()
-    fun company(id: ID): CompanyType? = TODO()
+class CompanyQuery(private val companyRepository: CompanyRepository) : Query {
+    fun allCompanies(): List<CompanyType> = companyRepository.findAll()
+        .map(::CompanyType)
+
+    fun company(id: ID): CompanyType? = companyRepository.findById(id.toLong())
+        ?.let(::CompanyType)
 }
--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/repository/CompanyRepository.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/repository/CompanyRepository.kt
@@ -1,6 +1,10 @@
 package com.okeicalm.simpleJournalEntry.repository
 
 import com.okeicalm.simpleJournalEntry.entity.Company
+import com.okeicalm.simpleJournalEntry.infra.db.tables.Companies
+import org.jooq.DSLContext
+import org.jooq.Field
+import org.springframework.stereotype.Repository
 
 interface CompanyRepository {
     fun findAll(): List<Company>
@@ -8,4 +12,30 @@ interface CompanyRepository {
     fun create(company: Company): Company
     fun update(company: Company): Company?
     fun delete(id: Long): Company?
+}
+
+@Repository
+class CompanyRepositoryImpl(private val dslContext: DSLContext) : CompanyRepository {
+    override fun findAll(): List<Company> = dslContext.select()
+        .from(Companies.COMPANIES)
+        .fetch()
+        .into(Company::class.java)
+
+    override fun findById(id: Long): Company? = dslContext
+        .fetchOne(Companies.COMPANIES, Companies.COMPANIES.ID eq id)
+        ?.into(Company::class.java)
+
+    override fun create(company: Company): Company {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(company: Company): Company? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: Long): Company? {
+        TODO("Not yet implemented")
+    }
+
+    private infix fun <T> Field<T>.eq(value: T) = this.eq(value)
 }
